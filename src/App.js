0537b90507e68d42be23735d236283874cd1f0a5
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-08-23 10:32:35 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ axios.defaults.withCredentials = true;
 const baseURL = 'http://localhost:3003';
 
 class App extends React.Component {
-<<<<<<< HEAD
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       modalIsOpen: false,
@@ -21,7 +20,8 @@ class App extends React.Component {
       displayName: '',
       currentUser: Cookies.get('user') || null,
       invalidLogin: false,
-      createQuiz: false
+      createQuiz: false,
+      showQuizzes: true
     };
 
     this.openModal = this.openModal.bind(this);
@@ -94,41 +94,83 @@ class App extends React.Component {
 
   async handleLogOut(event) {
     this.setState({ currentUser: null });
-    await axios.delete(`${baseURL}/sessions`, {});
+    const res = await axios.delete(`${baseURL}/sessions`, {});
   }
 
   showCreateQuiz = () => {
-    this.setState({ createQuiz: true });
+    this.setState({ createQuiz: true, showQuizzes: false });
   };
 
   hideCreateQuiz = () => {
-    this.setState({ createQuiz: false });
+    this.setState({ createQuiz: false, showQuizzes: true });
   };
 
   render() {
     return (
       <div>
-        <h1>The Knowledge</h1>
-        {this.state.currentUser !== null ? (
-          <button onClick={this.handleLogOut}>Log Out</button>
-        ) : (
-          <>
-            <button onClick={() => this.openModal('create')}>
-              Create Account
-            </button>
-            <button onClick={() => this.openModal('login')}>Log In</button>
-          </>
-        )}
+        <div className='header'>
+          <a href='/'>
+            <h1>The Knowledge</h1>
+          </a>
+          <div className='account'>
+            {this.state.currentUser && (
+              <>
+                {/* <p>Current User: {this.state.currentUser}</p> */}
+                {!this.state.createQuiz && (
+                  <button
+                    className='headerButton headerButtonImportant'
+                    onClick={this.showCreateQuiz}
+                  >
+                    Create Quiz
+                  </button>
+                )}
+              </>
+            )}
+            {this.state.createQuiz && (
+              <button
+                className='headerButton headerButtonImportantRed'
+                onClick={this.hideCreateQuiz}
+              >
+                Cancel Create Quiz
+              </button>
+            )}
+            {this.state.currentUser !== null ? (
+              <button className='headerButton' onClick={this.handleLogOut}>
+                Log Out
+              </button>
+            ) : (
+              <>
+                <button
+                  className='headerButton headerButtonImportant'
+                  onClick={() => this.openModal('create')}
+                >
+                  Create Account
+                </button>
+                <button
+                  className='headerButton'
+                  onClick={() => this.openModal('login')}
+                >
+                  Log In
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+
         {/* Create Account Modal */}
         <Modal
           isOpen={this.state.modalIsOpen}
           onRequestClose={this.closeModal}
           ariaHideApp={false}
+          className='modal'
+          overlayClassName='overlay'
         >
           {this.state.currentModal === 'create' ? (
             <>
-              <h2>Create Account</h2>
-              <button onClick={this.closeModal}>close</button>
+              <h2 className='modalTitle'>Create Account</h2>
+              <button className='closeModal' onClick={this.closeModal}>
+                close
+              </button>
               <p>Create an account in order to make your own quizzes!</p>
               <form onSubmit={this.handleCreateAccount}>
                 <input
@@ -160,8 +202,10 @@ class App extends React.Component {
             </>
           ) : (
             <>
-              <h2>Log In</h2>
-              <button onClick={this.closeModal}>close</button>
+              <h2 className='modalTitle'>Log In</h2>
+              <button className='closeModal' onClick={this.closeModal}>
+                close
+              </button>
               <form onSubmit={this.handleLogIn}>
                 <input
                   type='email'
@@ -185,253 +229,16 @@ class App extends React.Component {
             </>
           )}
         </Modal>
-        {this.state.currentUser && (
-          <>
-            <p>Current User: {this.state.currentUser}</p>
-            {!this.state.createQuiz && (
-              <button onClick={this.showCreateQuiz}>Create Quiz</button>
-            )}
-          </>
-        )}
-        {this.state.createQuiz && (
-          <button onClick={this.hideCreateQuiz}>Cancel Create Quiz</button>
-        )}
-        {this.state.createQuiz && <CreateQuiz baseURL={baseURL} />}
+        <div className='mainBodyParent'>
+          <div className='mainBody'>
+            {this.state.createQuiz && <CreateQuiz baseURL={baseURL} />}
+            {this.state.showQuizzes && <ShowAllQuizzes baseURL={baseURL} />}
+            <footer>Created by David &amp; the Peter's</footer>
+          </div>
+        </div>
       </div>
     );
   }
-=======
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			modalIsOpen: false,
-			currentModal: '',
-			email: '',
-			password: '',
-			displayName: '',
-			currentUser: Cookies.get('user') || null,
-			invalidLogin: false,
-			createQuiz: false,
-			showQuizzes: true
-		};
-
-		this.openModal = this.openModal.bind(this);
-		this.closeModal = this.closeModal.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-		this.handleCreateAccount = this.handleCreateAccount.bind(this);
-		this.handleLogIn = this.handleLogIn.bind(this);
-		this.handleLogOut = this.handleLogOut.bind(this);
-		this.showCreateQuiz = this.showCreateQuiz.bind(this);
-		this.hideCreateQuiz = this.hideCreateQuiz.bind(this);
-	}
-
-	openModal(whichModal) {
-		this.setState({ modalIsOpen: true, currentModal: whichModal });
-	}
-
-	closeModal() {
-		this.setState({
-			modalIsOpen: false,
-			invalidLogin: false,
-			email: '',
-			password: '',
-			displayName: ''
-		});
-	}
-
-	handleChange(event) {
-		this.setState({ [event.currentTarget.id]: event.currentTarget.value });
-	}
-
-	async handleCreateAccount(event) {
-		event.preventDefault();
-
-		await axios.post(`${baseURL}/users`, {
-			email: this.state.email,
-			password: this.state.password,
-			displayName: this.state.displayName
-		});
-
-		this.setState({
-			email: '',
-			password: '',
-			displayName: '',
-			modalIsOpen: false
-		});
-	}
-
-	async handleLogIn(event) {
-		event.preventDefault();
-
-		const response = await axios.post(`${baseURL}/sessions`, {
-			email: this.state.email,
-			password: this.state.password
-		});
-
-		if (response.data === 'wrong username or password') {
-			this.setState({
-				invalidLogin: true
-			});
-		} else {
-			this.setState({
-				email: '',
-				password: '',
-				currentUser: Cookies.get('user'),
-				modalIsOpen: false,
-				invalidLogin: false
-			});
-		}
-	}
-
-	async handleLogOut(event) {
-		this.setState({ currentUser: null });
-		const res = await axios.delete(`${baseURL}/sessions`, {});
-	}
-
-	showCreateQuiz = () => {
-		this.setState({ createQuiz: true, showQuizzes: false });
-	};
-
-	hideCreateQuiz = () => {
-		this.setState({ createQuiz: false, showQuizzes: true });
-	};
-
-	render() {
-		return (
-			<div>
-				<div className='header'>
-					<a href='/'>
-						<h1>The Knowledge</h1>
-					</a>
-					<div className='account'>
-						{this.state.currentUser && (
-							<>
-								{/* <p>Current User: {this.state.currentUser}</p> */}
-								{!this.state.createQuiz && (
-									<button
-										className='headerButton headerButtonImportant'
-										onClick={this.showCreateQuiz}
-									>
-										Create Quiz
-									</button>
-								)}
-							</>
-						)}
-						{this.state.createQuiz && (
-							<button
-								className='headerButton headerButtonImportantRed'
-								onClick={this.hideCreateQuiz}
-							>
-								Cancel Create Quiz
-							</button>
-						)}
-						{this.state.currentUser !== null ? (
-							<button className='headerButton' onClick={this.handleLogOut}>
-								Log Out
-							</button>
-						) : (
-							<>
-								<button
-									className='headerButton headerButtonImportant'
-									onClick={() => this.openModal('create')}
-								>
-									Create Account
-								</button>
-								<button
-									className='headerButton'
-									onClick={() => this.openModal('login')}
-								>
-									Log In
-								</button>
-							</>
-						)}
-					</div>
-				</div>
-
-				{/* Create Account Modal */}
-				<Modal
-					isOpen={this.state.modalIsOpen}
-					onRequestClose={this.closeModal}
-					ariaHideApp={false}
-					className='modal'
-					overlayClassName='overlay'
-				>
-					{this.state.currentModal === 'create' ? (
-						<>
-							<h2 className='modalTitle'>Create Account</h2>
-							<button className='closeModal' onClick={this.closeModal}>
-								close
-							</button>
-							<p>Create an account in order to make your own quizzes!</p>
-							<form onSubmit={this.handleCreateAccount}>
-								<input
-									type='email'
-									id='email'
-									name='email'
-									placeholder='email'
-									onChange={this.handleChange}
-									value={this.state.email}
-								/>
-								<input
-									type='password'
-									id='password'
-									name='password'
-									placeholder='password'
-									onChange={this.handleChange}
-									value={this.state.password}
-								/>
-								<input
-									type='text'
-									id='displayName'
-									name='displayName'
-									placeholder='display name'
-									onChange={this.handleChange}
-									value={this.state.displayName}
-								/>
-								<input type='submit' value='Create Account' />
-							</form>
-						</>
-					) : (
-						<>
-							<h2 className='modalTitle'>Log In</h2>
-							<button className='closeModal' onClick={this.closeModal}>
-								close
-							</button>
-							<form onSubmit={this.handleLogIn}>
-								<input
-									type='email'
-									id='email'
-									name='email'
-									placeholder='email'
-									onChange={this.handleChange}
-									value={this.state.email}
-								/>
-								<input
-									type='password'
-									id='password'
-									name='password'
-									placeholder='password'
-									onChange={this.handleChange}
-									value={this.state.password}
-								/>
-								<input type='submit' value='Log In' />
-								{this.state.invalidLogin && <p>Invalid Login, Try Again</p>}
-							</form>
-						</>
-					)}
-				</Modal>
-				<div className='mainBodyParent'>
-					<div className='mainBody'>
-						{this.state.createQuiz && <CreateQuiz baseURL={baseURL} />}
-						{this.state.showQuizzes && <ShowAllQuizzes baseURL={baseURL} />}
-						<footer>Created by David &amp; the Peter's</footer>
-					</div>
-				</div>
-			</div>
-		);
-	}
->>>>>>> 516deed7eedbbb669d46ee87351a041c8563bb20
 }
 
 export default App;
